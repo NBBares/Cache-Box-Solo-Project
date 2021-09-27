2fa1d8fa627efb907d7a8ef0cd616010f3dc8eea
Nick Bares <nick.b.bares@gmail.com>
2021-09-27 17:25:07 -0500

    GET project route updated working in postman, need to set up #tags still

--- server/routes/project.router.js
+++ server/routes/project.router.js
@@ -6,8 +6,10 @@ const router = express.Router();
  * GET route template
  */
  router.get('/project', (req, res) => {
-  const query = `SELECT * FROM "projects" 
-  JOIN "types" ON "types"."id" = "projects"."type_id";`
+  const query = `SELECT "projects"."id", "projects"."title", "projects"."project_description", "projects"."entry_date", "projects"."edit_date", "projects"."type_id", "projects"."user_id", "types"."type_name", jsonb_agg("images") as "images" FROM "projects" 
+  JOIN "types" ON "types"."id" = "projects"."type_id"
+  JOIN "images" ON "projects"."id" = "images"."project_id"
+  GROUP BY "projects"."id", "types"."id";`
   pool.query(query)
       .then(result => {
           res.send(result.rows);
