8791457af47a6e1cb17400451e9d7d62155bc532
Nick Bares <nick.b.bares@gmail.com>
2021-09-28 11:18:52 -0500

    GET all Working SERVER SIDE

--- server/routes/project.router.js
+++ server/routes/project.router.js
@@ -6,11 +6,18 @@ const router = express.Router();
  * GET route template
  */
  router.get('/project', (req, res) => {
-  const query = `SELECT "projects"."id", "projects"."title", "projects"."project_description", "projects"."entry_date", "projects"."edit_date", "projects"."type_id", "projects"."user_id", "types"."type_name", jsonb_agg("images") as "images" FROM "projects" 
+  const query = ` SELECT "projects"."id", "projects"."title", "projects"."project_description", "projects"."entry_date", "projects"."edit_date", "projects"."type_id", "projects"."user_id", "types"."type_name", "user"."id", jsonb_agg("tags") as "tags" ,jsonb_agg("images") as "images" FROM "projects" 
   JOIN "types" ON "types"."id" = "projects"."type_id"
   JOIN "images" ON "projects"."id" = "images"."project_id"
-  GROUP BY "projects"."id", "types"."id";`
-  pool.query(query)
+  JOIN "user" ON "user"."id" = "projects"."user_id"
+  JOIN "project_tags" ON "project_tags"."project_id" = "projects"."id"
+  JOIN "tags" ON "project_tags"."tag_id" = "tags"."id"
+  WHERE "projects"."user_id" = $1
+  GROUP BY "projects"."id", "types"."id", "user"."id", "project_tags"."project_id";`
+ 
+  console.log('req.user:', req.user.id);
+  if (req.isAuthenticated()) {
+  pool.query(query,[req.user.id])
       .then(result => {
           res.send(result.rows);
       })
@@ -18,6 +25,9 @@ const router = express.Router();
           console.log('ERROR: Get all items', err);
           res.sendStatus(500)
       })
+    }else {
+        res.sendStatus(403);
+      }
 });
 
 /**
