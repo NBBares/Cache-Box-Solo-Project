8e57c23eff374ca5e887c785beed30839cab56e3
Nick Bares <nick.b.bares@gmail.com>
2021-09-28 14:45:51 -0500

    WORKING DELETE

--- server/routes/project.router.js
+++ server/routes/project.router.js
@@ -33,8 +33,65 @@ const router = express.Router();
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
-  // POST route code here
-});
+ router.post('/post', (req, res) => {
+    console.log(req.body);
+    // RETURNING "id" will return project id
+    const insertProjectQuery = `
+    INSERT INTO "projects" ("title", "project_description", "type_id", "user_id")
+    VALUES ($1, $2, $3, $4)
+    RETURNING "id";`
+  
+    // FIRST QUERY MAKES Project
+    pool.query(insertProjectQuery, [req.body.title, req.body.project_description,req.body.type_id,req.body.user_id])
+    .then(result => {
+      console.log('New Project Id:', result.rows[0].id); //ID IS HERE!
+      
+      const createdProjectId = result.rows[0].id
+  
+      // Now handle the tags reference
+      const insertProjectTagQuery = `
+        INSERT INTO "project_tags" ("project_id", "tag_id")
+        VALUES  ($1, $2);
+        `
+        // SECOND QUERY ADDS TYPE FOR THAT NEW PROJECT
+        pool.query(insertProjectTagQuery, [createdProjectId, req.body.tag_id]).then(result => {
+          //Now that both are done, send back success!
+          const insertProjectImageQuery = `
+        INSERT INTO "images" ("project_id", "image_name", "image_description", "fav_image")
+        VALUES  ($1, $2, $3, $4);
+        `
+            // THIRD QUERY ADDS IMAGE FOR NEW PROJECT
+            pool.query(insertProjectImageQuery, [createdProjectId, req.body.image_name, req.body.image_description, req.body.fav_image ]).then(result => {
+                //Now that both are done, send back success!
+                res.sendStatus(201);
+            }).catch(err => {
+                // catch for third query
+                console.log(err);
+                res.sendStatus(500)
+            })
+        }).catch(err => {
+          // catch for second query
+          console.log(err);
+          res.sendStatus(500)
+        });
+  // Catch for first query
+    }).catch(err => {
+      console.log(err);
+      res.sendStatus(500)
+    })
+  })
 
+  //DELETE PROJECT
+  router.delete('/:id', (req, res) => {
+    //grab the project id
+    const queryText = 'DELETE FROM "projects" WHERE id=$1';
+    pool.query(queryText, [req.params.id])
+      .then(() => { res.sendStatus(200); })
+      //success
+      .catch((err) => {
+        console.log('Error completing SELECT Project query', err);
+        //catch for delete
+        res.sendStatus(500);
+      });
+  });
 module.exports = router;
