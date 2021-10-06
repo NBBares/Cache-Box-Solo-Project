c7672a9d6fc6c4d61f688deb490d6c240bd95992
Nick Bares <nick.b.bares@gmail.com>
2021-10-06 14:05:39 -0500

    Gallery Basic Bootstrap added

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -3,6 +3,10 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function GalleryPage() {
     const dispatch = useDispatch();
@@ -20,9 +24,49 @@ function GalleryPage() {
     return (
         <>
             <div className="container">
-
+            <Row xs={1} md={2} className="g-4">
+  {Array.from({ length: 4 }),projectData.map((projectItem, i) => (
+    <Col>
+      <Card onClick={() => history.push(`/description/${projectItem?.project_id}`)} key={i} data={projectItem?.id}>
+        <Card.Img variant="top" src={projectItem?.images[0]?.image_name} />
+        <Card.Body>
+          <Card.Title >{projectItem?.title}</Card.Title>
+          <Card.Text>Type:{projectItem?.type_name}</Card.Text>
+          <Card.Text>Tags:<ul>{(projectItem.tags.map((tags, i) => {
+                                return <li key={i} data={tags?.id}>{tags?.tag_name}</li>
+                            }))}</ul></Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  ))}
+</Row>
             </div>
-            <div>
+        </>
+    );
+}
+
+// this allows us to use <App /> in index.js
+export default GalleryPage;
+
+
+{/* <Row xs={1} md={2} className="g-4">
+  {Array.from({ length: 4 }).projectData.map((projectItem, i) => (
+    <Col>
+      <Card onClick={() => history.push(`/description/${projectItem?.project_id}`)} key={i} data={projectItem?.id}>
+        <Card.Img variant="top" src={projectItem?.images[0]?.image_name} />
+        <Card.Body>
+          <Card.Title >{projectItem?.title}</Card.Title>
+          <Card.Text>Type:{projectItem?.type_name}</Card.Text>
+          <Card.Text>Tags:<ul>{(projectItem.tags.map((tags, i) => {
+                                return <li key={i} data={tags?.id}>{tags?.tag_name}</li>
+                            }))}</ul></Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  ))}
+</Row> */}
+
+{/* <div>
                 <ul>
                     {projectData.map((projectItem, i) => {
                         return <li onClick={() => history.push(`/description/${projectItem?.project_id}`)} key={i} data={projectItem?.id}>Title:{projectItem?.title},
@@ -33,10 +77,4 @@ function GalleryPage() {
                         </li>;
                     })}
                 </ul>
-            </div>
-        </>
-    );
-}
-
-// this allows us to use <App /> in index.js
-export default GalleryPage;
+            </div> */}
